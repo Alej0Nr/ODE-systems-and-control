1c197c60b42a14afc20955b605a4fc7eba75290d
Alej0Nr <alejonicoramirez1@gmail.com>
2025-02-13 21:53:10 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -4,42 +4,35 @@ import matplotlib.pyplot as plt
 
 
 t = np.arange(-3, 3, .01)
+a = -1
+x1, y1 = np.exp(t), np.exp(a*t)
+c_i = np.arange(-5,5,1) 
 
-x1, y1 = np.exp(t), np.exp(-t)
+## plot diagrama de fase
+plt.subplot()
 
-x2, y2 = np.exp(t), np.exp(0*t)
-
-x3, y3 = np.exp(t), np.exp(t)
-
-Condiciones_Iniciales = [-5,-4,-3,-2,-1,0,1,2,3,4,5]
-
-
-plt.subplot(221)
-
-for c1 in Condiciones_Iniciales:
-    for c2 in Condiciones_Iniciales:
-        line, = plt.plot(c1*x1, c2*y1, lw=2, color='b')
+for c1 in c_i:
+    for c2 in c_i:
+        line, = plt.plot(c1*x1, c2*y1, lw=2, color='purple')
 plt.xlim(-5,5)
 plt.ylim(-5, 5)
 plt.grid(True)
-plt.xlabel("a=-1")
+plt.xlabel(f"a={a}")
 
-plt.subplot(222)
-for c1 in Condiciones_Iniciales:
-    for c2 in Condiciones_Iniciales:
-        line, = plt.plot(c1*x2, c2*y2, lw=2, color='r')
-plt.xlim(-5,5)
-plt.ylim(-5, 5)
-plt.grid(True)
-plt.xlabel("a=0")
-
-plt.subplot(223)
-for c1 in Condiciones_Iniciales:
-    for c2 in Condiciones_Iniciales:
-        line, = plt.plot(c1*x3, c2*y3, lw=2, color='green')
-plt.xlabel("a=1")
+### plot campo de direcciones
+plt.subplot()
+x = np.arange(-5,5,1)
+y = np.arange(-5,5,1)
+X, Y = np.meshgrid(x,y)
+dy= a*Y
+dx= X
+norm = np.sqrt(dx**2 + dy**2)
+dyu = dy/norm
+dxu = dx/norm
+plt.quiver(X,Y,dxu,dyu,color='gray')
 
 plt.xlim(-5,5)
 plt.ylim(-5, 5)
 plt.grid(True)
+
 plt.show()
