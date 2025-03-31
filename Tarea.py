de4f401cfe32dfee73f5cf053a600b367ffd825a
Alej0Nr <alejonicoramirez1@gmail.com>
2025-03-30 22:56:27 -0300

    Update Tarea.py

--- Tarea.py
+++ Tarea.py
@@ -1,22 +1,21 @@
 import sympy as sp
 
-"""
-las funciones por como estan escritas solo valen para cuando el sistema tiene un unico vector en nullspace
+def solve_M(A: sp.Matrix,B: sp.Matrix):
+    M = sp.Matrix(sp.BlockMatrix([sp.eye(A.shape[0]) - A,-B]))  
+    return M.nullspace()
 
-"""
-def solve_M(A,B):
-    M = sp.Matrix(sp.BlockMatrix([sp.eye(A.shape[0]) - A,-B]))
-    return sp.Matrix(M.nullspace())
 
 def eqpair(A,B):
-    x = solve_M(A,B)[:-B.shape[1]]
-    u = solve_M(A,B)[B.shape[0]:]
-    return x,u
-
+    espacioNulo = solve_M(A,B)
+    listaPares=[]
+    for v in espacioNulo:
+        listaPares.append((v[:-B.shape[1]],v[B.shape[0]:]))
+    return listaPares
 
 if __name__ == '__main__':
     A = sp.Matrix([[1,-1],[1/2,-1]])
+    # sp.pprint(A)
     B = sp.Matrix([-1,1])
-    # sp.pprint(solve_M(A,B))
-    print('El par de equilibro es', eqpair(A,B))
-
+    # sp.pprint(B)
+    for par in eqpair(A,B):
+        sp.pprint(par)
