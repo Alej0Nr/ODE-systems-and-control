51b0a780e60269e826f45f98914e47bca82b678f
Alej0Nr <alejonicoramirez1@gmail.com>
2025-05-02 02:46:15 -0300

    added SIR model

---
+++ ODES.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+
+class ODE:
+    def __init__(self, dxdt, dydt):
+        self.dxdt = dxdt
+        self.dydt = dydt
+        self.t_span = None
+        self.t_eval = None
+
+        
+    def sistema(self, t, z):
+        x, y = z
+        dxdt = self.dxdt(x, y)
+        dydt = self.dydt(x, y)
+        return [dxdt, dydt]
+    
+    def t_data(self, t_span, n):
+        self.t_span = t_span
+        self.t_eval = np.linspace(t_span[0], t_span[1], n)
+
+    def campo_direcciones(self, x_arange, y_arange, color = 'blue'):
+        X, Y = np.meshgrid(x_arange,y_arange)
+        dX = self.dxdt(X, Y)
+        dY = self.dydt(X, Y)
+        norm = np.sqrt(dX**2 + dY**2)
+        dXu = dX/norm
+        dYu = dY/norm
+        plt.quiver(X,Y,dXu,dYu,color = color)
+        plt.xlim(min(x_arange),max(x_arange))
+        plt.ylim(min(y_arange),max(y_arange))
+        plt.grid(True)
+    
+    def plano_temporal(self, z0):
+        solucion = solve_ivp(self.sistema , self.t_span, z0, t_eval= self.t_eval, method='BDF')
+        plt.plot(solucion.t, solucion.y[0])
+        plt.plot(solucion.t, solucion.y[1])
+        plt.xticks(np.arange(0, self.t_span[1]+1,2))
+        plt.grid(True)
+
+    def plano_fase(self, z0s):
+        for ci in z0s:
+            solucion = solve_ivp(self.sistema, self.t_span, ci, method='RK45', rtol= 1e-11)
+            plt.plot(solucion.y[0], solucion.y[1])
+        plt.grid(True)
+
+
+        
+
+def cis(n):
+    return [(i,i) for i in range(0,n,1)]
+
+
+def predador_presa():
+    #### Definimos el sistema Predador Presa
+    a, b, c, d = 0.4807, 0.02482, 0.02756, 0.9272
+    z0 = [30, 4]
+
+    lv = ODE(lambda x, y: x * (c * y - d), lambda x , y: y * (a - b * x))
+    lv.t_data((0, 20), 500)
+
+    #### ploteamos distintos graficos
+    lv.campo_direcciones(np.arange(0,40,1),np.arange(0,40,1))
+    plt.show()
+
+    lv.plano_temporal(z0)
+    plt.show()
+
+    lv.plano_fase(cis(20))
+    plt.show()
+
+
+# predador_presa()
+
+
+def ejemplo1():
+    #### definimos dy/dx = x**2 - y**2
+    ex = ODE(lambda x,y: 1, lambda x, y: x**2-y**2)
+    ex.campo_direcciones(np.arange(-5,6,.5),np.arange(-5,6,.5))
+    plt.show()
+
+    ex.t_data((0,2), 500)
+    z0 = [.0001,1]
+
+    ex.plano_fase([z0])
+    plt.show()
+    
+ejemplo1()
+
---
+++ SIR.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+
+
+def SIR(t, z, nu, beta, u=0):
+    s, i, r = z
+    dsdt = -(1-u)*beta*s*i
+    didt = (1-u)*beta*s*i - nu*i
+    drdt = nu*i
+
+    return [dsdt, didt, drdt]
+
+def plano_temporal(parameters, desde = None, hasta = None):
+    if desde == None or hasta == None:
+        solucion = solve_ivp(SIR, t_span, z0, t_eval= t_eval, args= parameters, method='RK45')
+        plt.plot(solucion.t, solucion.y[0], label='S(t)', color = 'blue')
+        plt.plot(solucion.t, solucion.y[1], label='I(t)', color = 'green')
+        plt.plot(solucion.t, solucion.y[2], label='R(t)', color = 'red')
+        print([solucion.y[0][-1],solucion.y[1][-1],solucion.y[2][-1]])
+    elif len(parameters)==3:
+        solucion1 = solve_ivp(SIR, (t_span[0],desde), z0, t_eval= np.linspace(t_span[0], desde, 1000), args= parameters[:-1], method='RK45')
+        z0n=[solucion1.y[0][-1],solucion1.y[1][-1],solucion1.y[2][-1]]
+        solucion2 = solve_ivp(SIR, (desde, hasta), z0n, t_eval= np.linspace(desde, hasta, 1000), args= parameters, method='RK45')
+        z0n=[solucion2.y[0][-1],solucion2.y[1][-1],solucion2.y[2][-1]]
+        solucion3 = solve_ivp(SIR, (hasta,t_span[1]), z0n, t_eval= np.linspace(hasta, t_span[1], 1000), args= parameters[:-1], method='RK45')
+        plt.plot(solucion1.t, solucion1.y[0], label='S(t)', color = 'blue')
+        plt.plot(solucion1.t, solucion1.y[1], label='I(t)', color = 'green')
+        plt.plot(solucion1.t, solucion1.y[2], label='R(t)', color = 'red')
+        plt.plot(solucion2.t, solucion2.y[0], color = 'blue')
+        plt.plot(solucion2.t, solucion2.y[1], color = 'green')
+        plt.plot(solucion2.t, solucion2.y[2], color = 'red')
+        plt.plot(solucion3.t, solucion3.y[0], color = 'blue')
+        plt.plot(solucion3.t, solucion3.y[1], color = 'green')
+        plt.plot(solucion3.t, solucion3.y[2], color = 'red')
+        plt.axvspan(desde,hasta, color='lightgray')
+    else:
+        return print("Recuerde ingresar un control")
+    
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+def plano_fase(parameters):
+    beta, nu, control = parameters
+    solucion = solve_ivp(SIR, (0,100), [999,1,0], t_eval= np.linspace(0, 100, 1000), args= parameters, method='RK45')
+    plt.plot(solucion.y[0], solucion.y[1])
+    plt.xlabel('S')
+    plt.ylabel('I')
+    plt.ylim(0,1000)
+    plt.grid()
+    plt.show()
+
+beta = 0.003
+nu = 1
+z0 = [999, 1, 0] 
+t_span = (0, 12)
+t_eval = np.linspace(t_span[0], t_span[1], 1000)
+
+"""si se desea solo graficar el sistema sin control use"""
+
+# plano_temporal([nu, beta])
+
+"""si desea ver como afecta un control use"""
+
+control = 0.3
+# plano_temporal([nu, beta, control])
+
+"""si desea ver como el control afecta en un intervalo de tiempo use"""
+
+plano_temporal([nu, beta, control], desde=2, hasta=4)
+
+
+
+
+""" ANDA MAL""" 
+# plano_fase([nu,beta,control])
+
+
