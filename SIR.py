ddc721749d5193409f1fa0558b4315e173a653a8
Alej0Nr <alejonicoramirez1@gmail.com>
2025-05-05 21:48:03 -0300

    improve SIR.py

--- SIR.py
+++ SIR.py
@@ -12,59 +12,73 @@ def SIR(t, z, nu, beta, u=0):
 
     return [dsdt, didt, drdt]
 
-def plano_temporal(parameters, desde = None, hasta = None):
+def plano_temporal(parameters, desde = None, hasta = None, t_span = (0, 12), z0 = [999,1,0]):
+    def ploter(intervalo , z0, control = False, add_label= False):
+        sol = solve_ivp(SIR, intervalo, z0, t_eval= np.linspace(intervalo[0], intervalo[1], np.ceil(intervalo[1]-intervalo[0])*10), args= parameters[:2] if control==False else parameters, method='RK45')
+        plt.plot(sol.t, sol.y[0], color = 'mediumblue', label = 'S(t)' if add_label else None)
+        plt.plot(sol.t, sol.y[1], color = 'green', label = 'I(t)' if add_label else None)
+        plt.plot(sol.t, sol.y[2], color = 'red', label = 'R(t)' if add_label else None)
+        z0=[sol.y[0][-1],sol.y[1][-1],sol.y[2][-1]]
+        return z0
     if desde == None or hasta == None:
-        solucion = solve_ivp(SIR, t_span, z0, t_eval= np.linspace(t_span[0], t_span[1], 1000), args= parameters, method='RK45')
-        plt.plot(solucion.t, solucion.y[0], label='S(t)', color = 'mediumblue')
-        plt.plot(solucion.t, solucion.y[1], label='I(t)', color = 'green')
-        plt.plot(solucion.t, solucion.y[2], label='R(t)', color = 'red')
+        ploter(t_span, z0, add_label= True, control=True)
+
     elif len(parameters)==3:
-        solucion1 = solve_ivp(SIR, (t_span[0],desde), z0, t_eval= np.linspace(t_span[0], desde, 1000), args= parameters[:-1], method='RK45')
-        z0n=[solucion1.y[0][-1],solucion1.y[1][-1],solucion1.y[2][-1]]
-        solucion2 = solve_ivp(SIR, (desde, hasta), z0n, t_eval= np.linspace(desde, hasta, 1000), args= parameters, method='RK45')
-        z0n=[solucion2.y[0][-1],solucion2.y[1][-1],solucion2.y[2][-1]]
-        solucion3 = solve_ivp(SIR, (hasta,t_span[1]), z0n, t_eval= np.linspace(hasta, t_span[1], 1000), args= parameters[:-1], method='RK45')
-        plt.plot(solucion1.t, solucion1.y[0], label='S(t)', color = 'mediumblue')
-        plt.plot(solucion1.t, solucion1.y[1], label='I(t)', color = 'green')
-        plt.plot(solucion1.t, solucion1.y[2], label='R(t)', color = 'red')
-        plt.plot(solucion2.t, solucion2.y[0], color = 'mediumblue')
-        plt.plot(solucion2.t, solucion2.y[1], color = 'green')
-        plt.plot(solucion2.t, solucion2.y[2], color = 'red')
-        plt.plot(solucion3.t, solucion3.y[0], color = 'mediumblue')
-        plt.plot(solucion3.t, solucion3.y[1], color = 'green')
-        plt.plot(solucion3.t, solucion3.y[2], color = 'red')
+        z0 = ploter((t_span[0],desde), z0, add_label= True)
+        z0 = ploter((desde,hasta), z0, control= True)
+        ploter((hasta, t_span[1]), z0)
+
         plt.axvspan(desde,hasta, color='lavender')
     else:
         return print("Recuerde ingresar un control")
     
+
     plt.legend()
     plt.grid()
     plt.show()
 
-def plano_fase(parameters, desde = None, hasta = None):
-
-    if desde == None:
-        solucion = solve_ivp(SIR, (0,100), [999,1,0], t_eval= np.linspace(0, 100, 1000), args= parameters, method='RK45')
-        plt.plot(solucion.y[0], solucion.y[1])
 
 
-    elif hasta == None:
-        solucion = solve_ivp(SIR, (0,100), [999,1,0], t_eval= np.linspace(0, desde, 1000), args= parameters[:-1], method='RK45')
+def plano_SI(parameters, ci = [999,1,0] , intervalo_tiempo = (0,12), intervalo_control = None, direcciones= False):
+    """
+    Grafica el plano SvsI con o sin intervalo de control, por default usa como condición inicial S0=999, I0=1 y R0=0.
+    
+    Por default, si se le pasa 3 parametros (nu, beta, control) se grafica el sistema controlado en todo el intervalo,
+    si se quiere controlar entre 2 y 4 use intervalo_control= (2,4)
+
+    Ejemplos:
+            plano_SI([nu,beta], direcciones=True)
+            plano_SI([nu,beta], intervalo_tiempo=(0,5))
+            plano_SI([nu,beta,control],direcciones= True)
+            plano_SI([nu,beta,control], intervalo_control= (2,4), intervalo_tiempo=(0,8))
+            plano_SI([nu,beta,control], intervalo_control= (2,5))
+    """
+    def ploter(z0, intervalo, control = False):
+        solucion = solve_ivp(SIR, intervalo, z0, t_eval= np.linspace(*intervalo, np.ceil((intervalo[1]-intervalo[0]))*10), args= parameters if control == True else parameters[:2], method='RK45')
         plt.plot(solucion.y[0], solucion.y[1], color= 'mediumblue')
-        z0c=[solucion.y[0][-1],solucion.y[1][-1],solucion.y[2][-1]]
-        solucion = solve_ivp(SIR, (desde,100), z0c, t_eval= np.linspace(desde, 100, 1000), args= parameters, method='RK45')
-        plt.plot(solucion.y[0], solucion.y[1], color = 'mediumblue')
-        plt.axvspan(z0c[0], solucion.y[0][-1], color='lavender')
+        z0 = [solucion.y[0][-1],solucion.y[1][-1],solucion.y[2][-1]]
+        return z0
+    
+    if intervalo_control == None:
+        ploter(ci, intervalo_tiempo, True)
+
     else:
-        solucion = solve_ivp(SIR, (0,100), [999,1,0], t_eval= np.linspace(0, desde, 1000), args= parameters[:-1], method='RK45')
-        plt.plot(solucion.y[0], solucion.y[1], color = 'mediumblue')
-        z0c=[solucion.y[0][-1],solucion.y[1][-1],solucion.y[2][-1]]
-        solucion = solve_ivp(SIR, (desde, hasta), z0c, t_eval= np.linspace(desde, hasta, 1000), args= parameters, method='RK45')
-        plt.plot(solucion.y[0], solucion.y[1], color = 'mediumblue')
-        z0n=[solucion.y[0][-1],solucion.y[1][-1],solucion.y[2][-1]]
-        solucion = solve_ivp(SIR, (hasta, 100), z0n, t_eval= np.linspace(hasta, 100, 1000), args= parameters[:-1], method='RK45')
-        plt.plot(solucion.y[0], solucion.y[1], color = 'mediumblue')
-        plt.axvspan(z0c[0], z0n[0], color='lavender')
+        z0 = ploter(ci, (intervalo_tiempo[0],intervalo_control[0]))
+        z0c = ploter(z0, (intervalo_control[0],intervalo_control[1]), control= True)
+        plt.axvspan(z0[0], z0c[0], color='lavender')
+        z0 = ploter(z0c, (intervalo_control[1], intervalo_tiempo[1]))
+    
+    if direcciones:
+        u = 0 if len(parameters)==2 else parameters[2]
+        nu, beta = parameters[:2]
+        S, I = np.meshgrid(np.arange(0,1000, 20),np.arange(0,1000, 20))
+        dsdt = -(1-u)*beta*S*I
+        didt = (1-u)*beta*S*I - nu*I
+        norm= np.hypot(dsdt,didt)
+        dSdt = dsdt/norm
+        dIdt = didt/norm
+        plt.quiver(S,I,dSdt,dIdt, color='lightgrey')
+        
     plt.xlabel('S')
     plt.ylabel('I')
     plt.ylim(0,1000)
@@ -72,20 +86,20 @@ def plano_fase(parameters, desde = None, hasta = None):
     plt.grid()
     plt.show()
 
+
+
+
 beta = 0.003
 nu = 1
 z0 = [999, 1, 0] 
-t_span = (0, 12)
-t_eval = np.linspace(t_span[0], t_span[1], 1000)
-
 """si se desea solo graficar el sistema sin control use"""
 
 # plano_temporal([nu, beta])
 
 """si desea ver como afecta un control use"""
 
-control = 0.30
-# plano_temporal([nu, beta, control])
+control = 0.34
+plano_temporal([nu, beta, control], t_span=(0,15))
 
 """si desea ver como el control afecta en un intervalo de tiempo use"""
 
@@ -93,13 +107,8 @@ control = 0.30
 
 
 
-
-""" si se desea graficar con o sin control use """ 
-# plano_fase([nu,beta])
-# plano_fase([nu,beta,control])
-
-""" si se desea graficar controlado en un intervalo use """
-# plano_fase([nu,beta,control], desde= 2)
-# plano_fase([nu,beta,control], desde= 2, hasta= 5)
-
-
+# plano_SI([nu,beta], direcciones=True)
+# plano_SI([nu,beta], intervalo_tiempo=(0,5))
+# plano_SI([nu,beta,control],direcciones= True)
+# plano_SI([nu,beta,control], intervalo_control= (2,4), intervalo_tiempo=(0,8))
+# plano_SI([nu,beta,control], intervalo_control= (2,5))
